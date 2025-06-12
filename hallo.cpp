5cbcc1ec24127cabd1a80dbc46d661af91d5c056
kipa5 <farisrydnr@gmail.com>
2025-06-12 12:13:46 +0800

    menampilkan data

--- hallo.cpp
+++ hallo.cpp
@@ -4,10 +4,15 @@ using namespace std;
 int main() {
     string nama[5]; // array untuk 5 nama
     return 0;
-    
+
     for (int i = 0; i < 5; i++) {
         cout << "Masukkan nama ke-" << i + 1 << ": ";
         cin >> nama[i];
+    }
+    
+     cout << "\nDaftar Nama:\n";
+    for (int i = 0; i < 5; i++) {
+        cout << "- " << nama[i] << endl;
     }
 
 }
