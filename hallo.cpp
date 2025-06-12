5045118bfb3bcc9e2aa4029c13f9972dc3742236
kipa5 <farisrydnr@gmail.com>
2025-06-12 12:17:55 +0800

    modifikasi kode

--- hallo.cpp
+++ hallo.cpp
@@ -2,17 +2,22 @@
 using namespace std;
 
 int main() {
+   cout << "Masukkan jumlah nama: ";
+    cin >> jumlah;
+
+    string* nama = new string[jumlah];
     string nama[5]; // array untuk 5 nama
-    return 0;
 
     for (int i = 0; i < 5; i++) {
         cout << "Masukkan nama ke-" << i + 1 << ": ";
         cin >> nama[i];
     }
-    
+
      cout << "\nDaftar Nama:\n";
     for (int i = 0; i < 5; i++) {
         cout << "- " << nama[i] << endl;
     }
 
+    delete[] nama;
+    return 0;
 }
