5482ea0be07a6c3e6af4e3255f7e3af25c5f7b24
kipa5 <farisrydnr@gmail.com>
2025-06-12 12:08:58 +0800

    input 5 nama

--- hallo.cpp
+++ hallo.cpp
@@ -4,4 +4,10 @@ using namespace std;
 int main() {
     string nama[5]; // array untuk 5 nama
     return 0;
+    
+    for (int i = 0; i < 5; i++) {
+        cout << "Masukkan nama ke-" << i + 1 << ": ";
+        cin >> nama[i];
+    }
+
 }
